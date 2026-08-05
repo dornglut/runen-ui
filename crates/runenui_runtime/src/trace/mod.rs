231e734f115017c1cdb57c6f2906919cc7502870
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-08-05 13:51:18 +0200

    Export normalized pointer cleanup trace types

--- crates/runenui_runtime/src/trace/mod.rs
+++ crates/runenui_runtime/src/trace/mod.rs
@@ -12,16 +12,16 @@ pub(crate) use admission::{MandatoryTracePlan, TraceReservation};
 pub(crate) use construction::TraceRecordDraft;
 pub use context::{
     TraceActionCategory, TraceActionIdentity, TraceCompositionContext, TraceCompositionRange,
-    TraceContext, TraceDeliveryOutcome, TraceEventContext, TraceEventFamily, TracePointerContext,
-    TracePointerPath, TracePublicationContext, TraceRouteSnapshot, TraceSurfaceContext,
-    TraceTargetTransition, TraceTextMetrics,
+    TraceContext, TraceDeliveryOutcome, TraceEventContext, TraceEventFamily, TracePointerCleanup,
+    TracePointerContext, TracePointerPath, TracePublicationContext, TraceRouteSnapshot,
+    TraceSurfaceContext, TraceTargetTransition, TraceTextMetrics,
 };
 pub use model::{
-    TraceConfig, TraceFocusBoundaryOutcome, TracePointerCaptureRequestRejection,
-    TracePointerRejection, TraceRecord, TraceRecordKind, TraceRoutedAdmissionRejection,
-    TraceRoutedIntegrityFailure, TraceSequence, TraceSpaceCleanupReason, TraceSurfaceIngressKind,
-    TraceSurfaceRejection, TraceSurfaceSnapshotKind, TraceTarget, TraceTargetRejection,
-    TraceTimerTerminalOutcome, TraceWorkFamily, TraceWorkIdentity, TraceWorkOwner,
-    TraceWorkStartRefusal,
+    TraceConfig, TraceFocusBoundaryOutcome, TracePointerCaptureRequestKind,
+    TracePointerCaptureRequestRejection, TracePointerRejection, TraceRecord, TraceRecordKind,
+    TraceRoutedAdmissionRejection, TraceRoutedIntegrityFailure, TraceSequence,
+    TraceSpaceCleanupReason, TraceSurfaceIngressKind, TraceSurfaceRejection,
+    TraceSurfaceSnapshotKind, TraceTarget, TraceTargetRejection, TraceTimerTerminalOutcome,
+    TraceWorkFamily, TraceWorkIdentity, TraceWorkOwner, TraceWorkStartRefusal,
 };
 pub use store::Trace;
